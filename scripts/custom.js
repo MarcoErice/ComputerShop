69966be3d6c705e854799d0af9c1b5c083f5c7e4
MarcoErice <marco.erice@gmail.com>
2017-02-13 13:24:25 +0100

    cuntom.js updated with Rajibs script

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,4 +1,35 @@
+$.ajax({
+    async: true,
+    url: 'https://webshop-ba3d.restdb.io/rest/project',
+    crossDomain: true,
+    dataType: 'json',
+    method: "GET",
+    headers: {
+        'x-apikey': "58a04ba554dd018673264282",
+        'content-type': 'application/json'
+    }
+})
 
+.done(successFunction)
+.fail(failFunction)
+
+function successFunction(data) {
+    var item = [];
+
+
+    $(data).each(function () {
+        item.push('<ul id="' + 'listUL' + '">' + '<li id="' + 'listLI' + '">' +
+        this.firstName + ' ' + this.lastName + '</br>' + this.email + '</br>' +
+         this.Citat + '</li>' + '</ul>');
+    });
+    item.push("</div>");
+    $("#mom").html(item.join(''));
+
+}
+
+function failFunction(request, textStatus, errorThrown) {
+    console.log('not work..');
+}
 
 
 
